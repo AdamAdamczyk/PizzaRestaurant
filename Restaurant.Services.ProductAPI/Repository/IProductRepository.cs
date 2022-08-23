e4cadb34698b7a75f1bbebd8fc9fbf4dff7789a2
Adam Adamczyk <adam.adamczyk@microsoft.wsei.edu.pl>
2022-08-23 19:37:08 +0200

    1.7 Dodanie metod do interfejsu

--- Restaurant.Services.ProductAPI/Repository/IProductRepository.cs
+++ Restaurant.Services.ProductAPI/Repository/IProductRepository.cs
@@ -1,6 +1,12 @@
-﻿namespace Restaurant.Services.ProductAPI.Repository
+﻿using Restaurant.Services.ProductAPI.Models;
+
+namespace Restaurant.Services.ProductAPI.Repository
 {
     public interface IProductRepository
     {
+        Task<IEnumerable<ProductDto>> HetProducts();
+        Task<ProductDto> GetProductById(int productId);
+        Task<ProductDto> CreateUpdateProduct(ProductDto productDto);
+        Task<bool> DeleteProduct(int productId);
     }
 }
