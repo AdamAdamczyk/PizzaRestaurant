b0d57c7c3c7586bff14d5793532c1efba52b9fe1
Adam Adamczyk <adam.adamczyk@microsoft.wsei.edu.pl>
2022-08-25 21:32:50 +0200

    1.10 Dodanie Controllera ProductAPI. HTTPGET

--- Restaurant.Services.ProductAPI/Program.cs
+++ Restaurant.Services.ProductAPI/Program.cs
@@ -2,6 +2,7 @@ using AutoMapper;
 using Microsoft.EntityFrameworkCore;
 using Restaurant.Services.ProductAPI;
 using Restaurant.Services.ProductAPI.DbContexts;
+using Restaurant.Services.ProductAPI.Repository;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +13,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 IMapper mapper = MappingConfig.Registermaps().CreateMapper();
 builder.Services.AddSingleton(mapper);
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
