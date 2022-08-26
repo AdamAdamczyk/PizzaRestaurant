02ebbdc483dbc7dc899e2c35308f1051b95dbf25
Adam Adamczyk <adam.adamczyk@microsoft.wsei.edu.pl>
2022-08-26 19:39:38 +0200

    1.13 Dodanie interfejsu dla produktów do głównego projektu

---
+++ Restaurant.Web/Models/ResponseDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Restaurant.Web.Models
+{
+    public class ResponseDto
+    {
+        public bool IsSuccess { get; set; } = true;
+        public object Result { get; set; }
+        public string DisplayMessege { get; set; } = "";
+        public List<string> ErrorMessages { get; set; }
+
+    }
+}
