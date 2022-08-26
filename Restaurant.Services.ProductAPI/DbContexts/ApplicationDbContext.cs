c505e12e209e629f02ae9fc12265d390c0d1a047
Adam Adamczyk <adam.adamczyk@microsoft.wsei.edu.pl>
2022-08-26 19:25:47 +0200

    1.12 dodanie pierwszych produktów

--- Restaurant.Services.ProductAPI/DbContexts/ApplicationDbContext.cs
+++ Restaurant.Services.ProductAPI/DbContexts/ApplicationDbContext.cs
@@ -13,6 +13,50 @@ namespace Restaurant.Services.ProductAPI.DbContexts
         {
 
         }
+
         public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                ProductId = 1,
+                Name = "Test 1",
+                Price = 15,
+                Description = "Test 1",
+                ImageUrl = "https://media.kaufland.com/images/PPIM/AP_Content_1010/std.lang.all/79/68/Asset_817968.jpg",
+                CategoryName = "Test 1"
+            });
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                ProductId = 2,
+                Name = "Test 2",
+                Price = 13.99,
+                Description = "Test 2",
+                ImageUrl = "https://media.kaufland.com/images/PPIM/AP_Content_1010/std.lang.all/79/68/Asset_817968.jpg",
+                CategoryName = "Test 2"
+            });
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                ProductId = 3,
+                Name = "Test 3",
+                Price = 10.99,
+                Description = "Test 3",
+                ImageUrl = "https://media.kaufland.com/images/PPIM/AP_Content_1010/std.lang.all/79/68/Asset_817968.jpg",
+                CategoryName = "Test 3"
+            });
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                ProductId = 4,
+                Name = "Test 4",
+                Price = 15,
+                Description = "Test 4",
+                ImageUrl = "https://media.kaufland.com/images/PPIM/AP_Content_1010/std.lang.all/79/68/Asset_817968.jpg",
+                CategoryName = "Test 4 "
+            });
+        }
     }
 }
---
+++ Restaurant.Services.ProductAPI/Migrations/20220826171559_NewProducts.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Restaurant.Services.ProductAPI.Migrations
+{
+    public partial class NewProducts : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "ProductId", "CategoryName", "Description", "ImageUrl", "Name", "Price" },
+                values: new object[,]
+                {
+                    { 1, "Test 1", "Test 1", "https://media.kaufland.com/images/PPIM/AP_Content_1010/std.lang.all/79/68/Asset_817968.jpg", "Test 1", 15.0 },
+                    { 2, "Test 2", "Test 2", "https://media.kaufland.com/images/PPIM/AP_Content_1010/std.lang.all/79/68/Asset_817968.jpg", "Test 2", 13.99 },
+                    { 3, "Test 3", "Test 3", "https://media.kaufland.com/images/PPIM/AP_Content_1010/std.lang.all/79/68/Asset_817968.jpg", "Test 3", 10.99 },
+                    { 4, "Test 4 ", "Test 4", "https://media.kaufland.com/images/PPIM/AP_Content_1010/std.lang.all/79/68/Asset_817968.jpg", "Test 4", 15.0 }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 4);
+        }
+    }
+}
