c8770430d916cca5eac6be4ee4eeceed4bf47859
Adam Adamczyk <adam.adamczyk@microsoft.wsei.edu.pl>
2022-09-06 16:27:14 +0200

    1.17 Dodanie pakietow Dodanie folderu identity instalacja duende

---
+++ Restaurant.Services.indetity/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Restaurant.Services.indetity.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
