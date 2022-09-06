e8938a6f968723b9cc50c8225c8535fd10ea8784
Adam Adamczyk <adam.adamczyk@microsoft.wsei.edu.pl>
2022-09-06 12:44:56 +0200

    1.16 Dodanie CRUD dla produktów

--- Restaurant.Web/Controllers/ProductController.cs
+++ Restaurant.Web/Controllers/ProductController.cs
@@ -8,20 +8,96 @@ namespace Restaurant.Web.Controllers
     public class ProductController : Controller
     {
         private readonly IProductService _productService;
+
         public ProductController(IProductService productService)
         {
             _productService = productService;
         }
-
         public async Task<IActionResult> ProductIndex()
         {
-            List<ProductDto> list = new();
+            var result = new List<ProductDto>();
             var response = await _productService.GetAllProductsAsync<ResponseDto>();
-            if(response!=null && response.IsSuccess)
+            if (response != null && response.IsSuccess)
             {
-                list = JsonConvert.DeserializeObject<List<ProductDto>>(Convert.ToString(response.Result));
+                result = JsonConvert.DeserializeObject<List<ProductDto>>(Convert.ToString(response.Result));
             }
-            return View(list);
+
+            return View(result);
+        }
+
+        public async Task<IActionResult> ProductCreate()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ProductCreate(ProductDto model)
+        {
+            if (ModelState.IsValid)
+            {
+                var response = await _productService.CreateProductAsync<ResponseDto>(model);
+                if (response != null && response.IsSuccess)
+                {
+                    return RedirectToAction(nameof(ProductIndex));
+                }
+            }
+
+            return View(model);
+        }
+
+        public async Task<IActionResult> ProductEdit(int productId)
+        {
+            var response = await _productService.GetProductByIdAsync<ResponseDto>(productId);
+            if (response != null && response.IsSuccess)
+            {
+                var result = JsonConvert.DeserializeObject<ProductDto>(Convert.ToString(response.Result));
+                return View(result);
+            }
+            return NotFound();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ProductEdit(ProductDto model)
+        {
+            if (ModelState.IsValid)
+            {
+                var response = await _productService.UpdateProductAsync<ResponseDto>(model);
+                if (response != null && response.IsSuccess)
+                {
+                    return RedirectToAction(nameof(ProductIndex));
+                }
+            }
+
+            return View(model);
+        }
+
+        public async Task<IActionResult> ProductDelete(int productId)
+        {
+            var response = await _productService.GetProductByIdAsync<ResponseDto>(productId);
+            if (response != null && response.IsSuccess)
+            {
+                var result = JsonConvert.DeserializeObject<ProductDto>(Convert.ToString(response.Result));
+                return View(result);
+            }
+            return NotFound();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ProductDelete(ProductDto model)
+        {
+            if (ModelState.IsValid)
+            {
+                var response = await _productService.DeleteProductAsync<ResponseDto>(model.ProductId);
+                if (response != null && response.IsSuccess)
+                {
+                    return RedirectToAction(nameof(ProductIndex));
+                }
+            }
+
+            return View(model);
         }
     }
 }
